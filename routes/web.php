e0a89e79c4201c59da4c54a14548942dfee4e129
Sheikh Ali <ali.professional373@gmail.com>
2024-09-06 20:18:12 +0500

    Creating routes and views

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/hello', function () {
+//     return view('home');
+// });
+
+// Route::view('/hello', 'home');
+
+// Route::get('/hello/{name?}/comment/{comment?}', function ($name = null, $comm = null) {
+//     if($name){
+//         return "<h1>Hello, $name </h1><br> $comm";
+//     }else{
+//         return "<h1>No name</h1>";
+//     }
+// })->whereAlpha('name', 'comment');
+
+// Route::get('/hello/{name?}/comment/{comment?}', function ($name = null, $comm = null) {
+//     if($name){
+//         return "<h1>Hello, $name </h1><br> $comm";
+//     }else{
+//         return "<h1>No name</h1>";
+//     }
+// })->whereIn('name', ['ali', 10]);
+
+// Route::get('/hello/{name?}/comment/{comment?}', function ($name = null, $comm = null) {
+//     if($name){
+//         return "<h1>Hello, $name </h1><br> $comm";
+//     }else{
+//         return "<h1>No name</h1>";
+//     }
+// })->where('name', '[0-9]+')->whereAlpha('comment'); // create Regex for whatever you want to be accepted in the URL
+
+// Named Routes
+Route::get('/hello', function(){
+   return 'home'; 
+})->name('hello'); // if we have called this route in an anchor tag at some page, then we can use route('hello'), to make a call to this route/ URL
